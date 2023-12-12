4de5fd6ee1a417b495c514ade930d2c33c24e41f
spizzy <127999573+Spizzyman@users.noreply.github.com>
2023-12-12 22:13:28 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['spizzy', 'spizzybot', true],
+  ['spizz', 'spizzybot', true],
   ['6281255369012', 'spizzy', true], 
   [''] 
 ] //Number of owners
